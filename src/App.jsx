3a8d89c793f379199c14c4570fecf5ef9b6f9915
Punsakorn Aroonchote <bright007456@gmail.com>
2025-10-21 15:23:31 +0700

    refactor: remove ArticleSection from App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Navbar from './components/Navbar.jsx'
 import HeroSelection from './components/HeroSelection.jsx'
 import Footer from './components/Footer.jsx'
-import ArticleSection from './components/ArticleSection.jsx'
 
 
 
@@ -11,7 +10,6 @@ function App() {
     <>
     <Navbar />
     <HeroSelection />
-    <ArticleSection />
     <Footer />
     </>
   )
