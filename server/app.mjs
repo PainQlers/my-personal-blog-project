3956907902ee6b4b30085290c881cf9807377589
Punsakorn Aroonchote <bright007456@gmail.com>
2025-10-23 17:14:02 +0700

    refactor: import Supabase client and dotenv configuration for improved database access

--- server/app.mjs
+++ server/app.mjs
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import express from "express";
 import postsRouter from './routes/posts.mjs';
 import cors from "cors";
 import authRouter from "./routes/auth.mjs";
+import { supabase } from './utils/db.mjs';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -27,11 +29,6 @@ app.get("/api/test" , (req, res) => {
 
 app.get("/api/test1" , async (req, res) => {
     try {
-        const { createClient } = await import('@supabase/supabase-js');
-        const supabaseUrl = 'https://dngbgajbvfpzvskalouc.supabase.co';
-        const supabaseKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZ2JnYWpidmZwenZza2Fsb3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5MTM1ODAsImV4cCI6MjA3NjQ4OTU4MH0.uR2QWo4mHjzWmJXNz0wo2CE-YQqHwI6Ug5ymwvfiBys';
-        const supabase = createClient(supabaseUrl, supabaseKey);
-        
         const { data, error } = await supabase.from('users').select('*');
         if (error) return res.status(500).json({ error: error.message });
         return res.json(data);
